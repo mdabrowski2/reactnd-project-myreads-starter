3dc674fb8dc54d01a169e8ec44be22c7b74b601e
Mateusz Dąbrowski <mateusz.dabrowski.1994@gmail.com>
2020-05-24 10:59:11 +0200

    added searchBooks method

--- src/views/SearchPage.js
+++ src/views/SearchPage.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from '../BooksAPI';
 
 class SearchPage extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            fetchedBooks: []
+        };
+    }
+
+    async searchBooks(query) {
+        if (!query) {
+            return;
+        }
+
+        const response = await BooksAPI.search(query)
+
+        if (Array.isArray(response.books)) {
+            this.setState({
+                fetchedBooks: response.books
+            });
+        }
+    }
+
     render() {
         return <div className="search-books">
             <div className="search-books-bar">
@@ -17,7 +40,7 @@ class SearchPage extends React.Component {
                     However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                     you don't find a specific author or title. Every search is limited by search terms.
                     */}
-                    <input type="text" placeholder="Search by title or author"/>
+                    <input type="text" onChange={event => this.searchBooks(event.target.value)} placeholder="Search by title or author"/>
 
                 </div>
             </div>
